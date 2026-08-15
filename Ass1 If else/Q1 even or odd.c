359855fd68efc0b01bed7304b6e06ebb2917c9ed
kadamom20 <kadamom262@gmail.com>
2026-08-15 23:24:44 +0530

    Add files via upload

---
+++ Ass1 If else/Q1 even or odd.c
@@ -0,0 +1,10 @@
+void main()
+{
+	int num;
+	printf("Enter a number:");
+	scanf("%d",&num);
+	if(num%2==0)
+	 printf("Number is even");
+	else
+	 printf("Number is odd");
+}
---
+++ Ass1 If else/Q5 eligible to vote.c
@@ -0,0 +1,12 @@
+void main()
+{
+	int age=21;
+	if(age>=18)
+	{
+	printf("Person is eligible to vote");	
+	}
+	else
+	{
+		printf("Person is not eligible to vote");
+	}
+}
