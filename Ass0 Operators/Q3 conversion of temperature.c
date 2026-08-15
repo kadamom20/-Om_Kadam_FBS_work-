1bf8f30f0685330394c4377f8e4468f42bed7317
kadamom20 <kadamom262@gmail.com>
2026-08-15 23:12:20 +0530

    Add files via upload

---
+++ Ass0 Operators/Q3 conversion of temperature.c
@@ -0,0 +1,7 @@
+void main()
+{
+	float c,f;
+	c=10;
+	f=(c*9/5)+32;
+	printf("Temperature in Fahrenheit is %f",f);
+}
---
+++ Ass0 Operators/Q6 square cube of number.c
@@ -0,0 +1,9 @@
+void main()
+{
+	int num,square,cube;
+	num=10;
+	square=num*num;
+	cube=num*num*num;
+	printf("Square=%d\n",square);
+	printf("cube=%d",cube);
+}
